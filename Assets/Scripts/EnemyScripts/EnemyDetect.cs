e04101a93ce12c6d742d513e52ae5ea582d5702d
Ajdin Talic <89469945+MagmarRager@users.noreply.github.com>
2022-11-22 16:52:51 +0100

    Added Personal Scene And Enemy Prefab
    Enemy Prefab with only working player detection so far!

---
+++ Assets/Scripts/EnemyScripts/EnemyDetect.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyDetect : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private GameObject player;
+
+    public LayerMask detectableLayers;
+
+    [SerializeField]
+    private bool playerVisable = false;
+
+    private bool playerInViewRange = false;
+
+    private int colliderIndex;
+
+    private void Update()
+    {
+        if (playerInViewRange)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, 14, detectableLayers);
+            Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+
+            if (hit.collider.gameObject.layer == 3)
+            {
+                playerVisable = false;
+            }
+            else
+                playerVisable = true;
+
+            Debug.Log(hit.collider.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerInViewRange = true;
+            player = collision.gameObject;
+            colliderIndex++;
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            colliderIndex--;
+            if (colliderIndex == 0)
+            {
+                playerInViewRange = false;
+                playerVisable = false;
+                player = null;
+            }
+        }
+    }
+}
