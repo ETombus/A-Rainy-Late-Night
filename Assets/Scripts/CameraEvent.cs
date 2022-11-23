cee221581af52e430cb6cf5c5974f436a2ac4f33
AndStyver <andsty0125@skola.goteborg.se>
2022-11-23 13:48:36 +0100

    added a camera event and shooting script
    also added some graphical assets (balcony, enemy)

---
+++ Assets/Scripts/CameraEvent.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraEvent : MonoBehaviour
+{
+    [SerializeField] Transform cameraPos;
+    [SerializeField] CinemachineVirtualCamera vCam;
+
+    Transform cameraOrigin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPos = this.gameObject.transform.GetChild(0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            vCam.Follow = cameraPos;
+            cameraOrigin = collision.transform.GetChild(0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        vCam.Follow = cameraOrigin;
+    }
+}
