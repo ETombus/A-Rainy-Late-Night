2e8bd56bac8a9c2e8c2da81e13611434d85ca6e7
AndStyver <andsty0125@skola.goteborg.se>
2022-12-12 13:24:49 +0100

    tutorial level first draft

--- Assets/Scripts/Player/PlayerSoundHandler.cs
+++ Assets/Scripts/Player/PlayerSoundHandler.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spine.Unity;
+using Spine;
 
 public class PlayerSoundHandler : MonoBehaviour
 {
     AudioSource audSource;
+    SkeletonAnimation skelAnim;
+
+    [SerializeField] AudioClip[] clips;
+    //0 = footstep
 
     private void Start()
     {
         audSource = GetComponent<AudioSource>();
+        skelAnim = GetComponentInChildren<SkeletonAnimation>();
+
+        skelAnim.state.Event += StateEvent;
+    }
+
+    private void StateEvent(TrackEntry trackEntry, Spine.Event e)
+    {
+        if (e.Data.Name == "footsteps")
+        {
+            PlaySound(clips[0]);
+        }
     }
 
     public void PlaySound(AudioClip clip)
