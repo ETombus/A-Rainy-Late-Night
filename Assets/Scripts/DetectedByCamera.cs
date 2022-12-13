89e0d03debad95d8b0dfd27e3ed9bc025dd7899a
Ajdin Talic <89469945+MagmarRager@users.noreply.github.com>
2022-12-13 15:08:05 +0100

    Marker fixed, text and selected added

--- Assets/Scripts/DetectedByCamera.cs
+++ Assets/Scripts/DetectedByCamera.cs
@@ -1,6 +1,7 @@
 using Spine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -13,11 +14,41 @@ public class DetectedByCamera : MonoBehaviour
     [Range(0f, 185f)]
     public float distanceFromPlayer = 100;
 
-    public string PopupText;
+    [Header("Hook Variables")]
+    public bool isAHookPoint = false;
+    public bool isSelected = false;
+
+    [Header("Text Varibles")]
+    public bool ShouldHaveText = false;
+    public string textToWrite;
+
+    public float timePerCharacter = 0.2f;
+    public float timePerRemoval = 0.05f;
+
+    private TextMeshProUGUI uiText;
+    private Canvas canvas;
+
+    int characterIndex;
+    float timer;
+
+    private bool writingText = false;
+    private bool erasingText = false;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        canvas = GetComponentInChildren<Canvas>();
+
+        if (ShouldHaveText)
+        {
+            canvas.enabled = true;
+            uiText = GetComponentInChildren<TextMeshProUGUI>();
+            uiText.text = "";
+        }
+        else
+        {
+            canvas.enabled = false;
+        }
 
     }
 
@@ -32,6 +63,20 @@ public class DetectedByCamera : MonoBehaviour
         {
             DisableMarker();
         }
+
+        if(isAHookPoint)
+        {
+            if (isSelected)
+                animator.SetBool("Selected", true);
+            else
+                animator.SetBool("Selected", false);
+        }
+
+
+        if (writingText)
+            WriteText();
+        else if (erasingText)
+            EraseText();
     }
 
     public bool IsVisibleToCamera()
@@ -47,17 +92,61 @@ public class DetectedByCamera : MonoBehaviour
     {
         lookingAtIt = true;
         animator.SetTrigger("ResetAnim");
-        if (PopupText != null || PopupText == "")
-        {
-            ScannerController.Instance.WriteText(PopupText, 0.1f);
-            Debug.Log("Write "+PopupText );
-        }
 
+        if (ShouldHaveText)
+        {
+            writingText = true;
+            erasingText = false;
+        }
     }
 
     void DisableMarker()
     {
         lookingAtIt = false;
         animator.SetTrigger("RemoveMarker");
+
+        if (ShouldHaveText)
+        {
+            if (characterIndex > 0)
+            {
+                writingText = false;
+                erasingText = true;
+            }
+        }
     }
+
+    void WriteText()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer += timePerCharacter;
+            characterIndex++;
+            uiText.text = textToWrite.Substring(0, characterIndex);
+
+            if (characterIndex >= textToWrite.Length)
+            {
+                writingText = false;
+                Debug.Log("Done Writing");
+            }
+        }
+    }
+
+    void EraseText()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            timer += timePerRemoval;
+            characterIndex--;
+            uiText.text = textToWrite.Substring(0, characterIndex);
+            if (characterIndex <= 0)
+            {
+                erasingText = false;
+                Debug.Log("Done Erasing");
+            }
+        }
+    }
+
+
 }
