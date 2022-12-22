870792139214fea4558210c457ee47b8db39dcdb
Ajdin Talic <89469945+MagmarRager@users.noreply.github.com>
2022-12-22 15:14:23 +0100

    AlleyWayScene worker

--- Assets/Scripts/Player/Healthbar.cs
+++ Assets/Scripts/Player/Healthbar.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEditor.SceneManagement;
 
 public class Healthbar : HealthHandler
 {
--- Assets/Scripts/Universal/DevTools.cs
+++ Assets/Scripts/Universal/DevTools.cs
@@ -6,62 +6,62 @@ using UnityEngine.SceneManagement;
 
 public class DevTools : MonoBehaviour
 {
-    PlayerInputs playerControls;
-    private InputAction teleport;
-    private InputAction spawnBarrel;
-    private InputAction resetScene;
+    //PlayerInputs playerControls;
+    //private InputAction teleport;
+    //private InputAction spawnBarrel;
+    //private InputAction resetScene;
 
-    public GameObject teleportObject;
-    public GameObject spawnObject;
+    //public GameObject teleportObject;
+    //public GameObject spawnObject;
 
-    private Vector2 mousePos;
-
-
-    private void Awake()
-    {
-        playerControls = new PlayerInputs();
-    }
-
-    private void Update()
-    {
-        mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-    }
-
-    private void OnEnable()
-    {
-        teleport = playerControls.Player.DevToolTeleport;
-        spawnBarrel = playerControls.Player.DevToolSpawnBarrel;
-        resetScene = playerControls.Player.DevToolResetScene;
+    //private Vector2 mousePos;
 
 
-        teleport.Enable();
-        spawnBarrel.Enable();
-        resetScene.Enable();
+    //private void Awake()
+    //{
+    //    playerControls = new PlayerInputs();
+    //}
+
+    //private void Update()
+    //{
+    //    mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+    //}
+
+    //private void OnEnable()
+    //{
+    //    teleport = playerControls.Player.DevToolTeleport;
+    //    spawnBarrel = playerControls.Player.DevToolSpawnBarrel;
+    //    resetScene = playerControls.Player.DevToolResetScene;
 
 
-        teleport.performed += TeleportPlayer;
-        spawnBarrel.performed += SpawnBarrelAtMouse;
-        resetScene.performed += ResetScene;
-    }
+    //    teleport.Enable();
+    //    spawnBarrel.Enable();
+    //    resetScene.Enable();
 
-    private void OnDisable()
-    {
-        teleport.Disable();
-        spawnBarrel.Disable();
-        resetScene.Disable();
-    }
 
-    private void TeleportPlayer(InputAction.CallbackContext context)
-    {
-        teleportObject.transform.position = mousePos;
-    }
-    private void SpawnBarrelAtMouse(InputAction.CallbackContext context)
-    {
-        Instantiate(spawnObject, mousePos, Quaternion.identity);
-    }
-    private void ResetScene(InputAction.CallbackContext context)
-    {
-        Scene scene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(scene.name);
-    }
+    //    teleport.performed += TeleportPlayer;
+    //    spawnBarrel.performed += SpawnBarrelAtMouse;
+    //    resetScene.performed += ResetScene;
+    //}
+
+    //private void OnDisable()
+    //{
+    //    teleport.Disable();
+    //    spawnBarrel.Disable();
+    //    resetScene.Disable();
+    //}
+
+    //private void TeleportPlayer(InputAction.CallbackContext context)
+    //{
+    //    teleportObject.transform.position = mousePos;
+    //}
+    //private void SpawnBarrelAtMouse(InputAction.CallbackContext context)
+    //{
+    //    Instantiate(spawnObject, mousePos, Quaternion.identity);
+    //}
+    //private void ResetScene(InputAction.CallbackContext context)
+    //{
+    //    Scene scene = SceneManager.GetActiveScene();
+    //    SceneManager.LoadScene(scene.name);
+    //}
 }
