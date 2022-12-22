6bd6c7b8238aba01c98474123bd533dbac1a2a90
Ajdin Talic <89469945+MagmarRager@users.noreply.github.com>
2022-12-22 14:35:49 +0100

    dasdsa

--- Assets/Scripts/Universal/PlayAnimation.cs
+++ Assets/Scripts/Universal/PlayAnimation.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Animator))]
+
 public class PlayAnimation : MonoBehaviour
 {
     private Animator anim;
     public string animationTriggerName;
 
-    private void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
+    public Sprite newSprite;
+
+
+
     public void PlayAnimationClip()
     {
+        try
+        {
+        anim = GetComponent<Animator>();
         anim.SetTrigger(animationTriggerName);
+
+        }
+        catch
+        {
+            Debug.LogWarning("Warning: Missing Animator on " + gameObject);
+        }
+    }
+
+    public void ChangeSprite()
+    {
+        try
+        {
+           gameObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+        }
+        catch
+        {
+            Debug.LogWarning("Warning: Missing SpriteRenderer or Sprite on " + gameObject);
+        }
     }
 
 }
