158f3a50202d033704e27db98ffb4f6fe4b9bfa0
AndStyver <andsty0125@skola.goteborg.se>
2022-12-27 09:09:40 +0100

    created a basic glass shatter particle

--- Assets/Scripts/Universal/Destructible.cs
+++ Assets/Scripts/Universal/Destructible.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
+    [SerializeField] GameObject shatter;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Slice"))
         {
             Destroy(gameObject);
+
+            if (shatter != null)
+                Instantiate(shatter, transform.position, transform.rotation);
         }
     }
 }
