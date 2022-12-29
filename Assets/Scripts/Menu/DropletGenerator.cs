e6e32e27d719c154529c91735689cc881587d268
AndStyver <andsty0125@skola.goteborg.se>
2022-12-29 15:57:28 +0100

    changed droplets to randomize positions
    also they wont play unless theyre finished playing to prevent spazzing droplets

--- Assets/Scripts/Menu/DropletGenerator.cs
+++ Assets/Scripts/Menu/DropletGenerator.cs
@@ -8,9 +8,12 @@ public class DropletGenerator : MonoBehaviour
     [SerializeField] GameObject dropletsParent;
     [SerializeField] List<GameObject> droplets;
     List<float> dropletTimers;
+    List<Vector2> startPos;
+    List<float> posOffset;
 
     [SerializeField] float minTime = .2f;
     [SerializeField] float maxTime = 1f;
+    [SerializeField] float maxOffset;
 
     //[SerializeField] GameObject[] dropletObject;
     [SerializeField] RuntimeAnimatorController[] animators;
@@ -29,9 +32,21 @@ public class DropletGenerator : MonoBehaviour
             dropletTimers.Add(0f);
         }
 
+        posOffset = new List<float>();
+        for (int i = 0; i < dropletTimers.Count; i++)
+        {
+            posOffset.Add(0f);
+        }
+
         for (int i = 0; i < dropletTimers.Count; i++)
         {
             dropletTimers[i] = Random.Range(minTime, maxTime);
+        }
+
+        startPos = new List<Vector2>();
+        for (int i = 0; i < posOffset.Count; i++)
+        {
+            startPos.Add(dropletsParent.transform.GetChild(i).position);
         }
     }
 
@@ -40,9 +55,11 @@ public class DropletGenerator : MonoBehaviour
         for (int i = 0; i < dropletTimers.Count; i++)
         {
             dropletTimers[i] -= Time.deltaTime;
-            if (dropletTimers[i] <= 0)
+            if (dropletTimers[i] <= 0 && droplets[i].activeSelf == false)
             {
                 droplets[i].GetComponent<Animator>().runtimeAnimatorController = animators[Random.Range(0, animators.Length)];
+                posOffset[i] = Random.Range(-maxOffset, maxOffset);
+                droplets[i].transform.position = new(startPos[i].x + posOffset[i], startPos[i].y + posOffset[i]);
                 droplets[i].SetActive(true);
                 dropletTimers[i] = dropletTimers[i] = Random.Range(minTime, maxTime);
             }
