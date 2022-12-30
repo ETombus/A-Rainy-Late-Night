bd9f0668db75c09be0a73805ef792dc92944a8fc
Ajdin Talic <89469945+MagmarRager@users.noreply.github.com>
2022-12-30 13:45:18 +0100

    Revert "Water changes"
    This reverts commit 9824c5954017cd9614062ebcc5029f01b072e0d4.

--- Assets/Scripts/Universal/DamageOverTime.cs
+++ Assets/Scripts/Universal/DamageOverTime.cs
@@ -4,42 +4,9 @@ using UnityEngine;
 
 public class DamageOverTime : MonoBehaviour
 {
-    private Healthbar playerHealth;
+    
 
-    public float damageInterval = 0.2f;
-    public float damageAmmount = 1;
-    private bool dealDamage = false;
 
-    float timer = 0;
-
-    private void Update()
-    {
-        if (timer > 0)
-        {
-            timer -= Time.deltaTime;
-        }
-        else if (dealDamage)
-        {
-            timer = damageInterval;
-            playerHealth.ReduceHealth(damageAmmount, true);
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            playerHealth = collision.GetComponent<Healthbar>();
-            dealDamage = true;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.tag == "Player")
-        {
-            dealDamage = false;
-        }
-    }
 
 
 }
