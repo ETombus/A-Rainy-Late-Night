7ff89c2cd160ba745920131037111a6840376fb4
AndStyver <andsty0125@skola.goteborg.se>
2023-01-03 12:44:06 +0100

    updated checkpoints

--- Assets/Scripts/Universal/Checkpoint.cs
+++ Assets/Scripts/Universal/Checkpoint.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [Header("Checkpoint")]
     public int index = 1; //index 0 is the starting position for the player, the first checkpoint should be 1
     public bool reached = false;
 
+    [Header("Settings")]
     [SerializeField] bool useStandardPos = true; //if true, respawnposition gets set to the checkpoints position
     public Vector2 respawnPos;
 
     CheckpointManager manager;
 
+    [Header("Audio")]
+    [SerializeField] AudioClip clip;
+    AudioSource audSource;
+    
+
     private void Start()
     {
         manager = GameObject.Find("GameManager").GetComponent<CheckpointManager>();
+        audSource = GetComponent<AudioSource>();
 
         if (useStandardPos)
             respawnPos = transform.position;
@@ -24,10 +32,11 @@ public class Checkpoint : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player") && !reached)
         {
-            collision.gameObject.GetComponent<Healthbar>().AddHealth(100);
+            collision.gameObject.GetComponent<Healthbar>().AddHealth(100, true);
 
             reached = true;
             manager.UpdateCheckpoints(index);
+            audSource.PlayOneShot(clip);
         }
     }
 
