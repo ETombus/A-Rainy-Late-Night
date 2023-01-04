3970cbc8e5f526e87e8d626fcf737629082a6e1a
AndStyver <andsty0125@skola.goteborg.se>
2023-01-04 17:36:57 +0100

    Update OutroCutsceneManager.cs

--- Assets/OutroCutsceneManager.cs
+++ Assets/OutroCutsceneManager.cs
@@ -57,6 +57,8 @@ public class OutroCutsceneManager : MonoBehaviour
             if (music != null)
                 music.FadeClip(MusicManager.GameScene.Outro, true);
 
+            PlayerPrefs.SetInt("PlayIntroAnimation", 0); //1 = false, 0 = true
+
             pauseManager.enabled = false;
 
             cutsceneStarted = true;
